a4aec1fa802c8c864868b6842a7bb6ab84dde1c9
Gary Dinh <16717647+vietthang207@users.noreply.github.com>
2024-04-04 20:13:50 +0200

    add tls

--- network/Labsetup/volumes/tun_client.py
+++ network/Labsetup/volumes/tun_client.py
@@ -5,6 +5,7 @@ import struct
 import os
 import time
 from scapy.all import *
+import ssl
 
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
@@ -16,36 +17,59 @@ PROTO_NUMBER_ICMP = 1
 CLIENT_IP="10.9.0.5"
 CLIENT_PORT = 9090
 CLIENT_TUN_GATEWAY="192.168.53.99"
-
 SERVER_IP="10.9.0.11"
 SERVER_PORT=9090
 SERVER_TUN_GATEWAY="192.168.53.99"
-
 PRIVATE_NETWORK_SUBNET="192.168.60.0/24"
+
+PATH_CERT_FILE="cert.pem"
+PATH_KEY_FILE="key.pem"
 
 # Create the tun interface
 tun = os.open("/dev/net/tun", os.O_RDWR)
 ifr = struct.pack('16sH', b'tun%d', IFF_TUN | IFF_NO_PI)
 ifname_bytes  = fcntl.ioctl(tun, TUNSETIFF, ifr)
-
-# Get the interface name
 ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
 print("Interface Name: {}".format(ifname))
-
 os.system("ip addr add {}/24 dev {}".format(CLIENT_TUN_GATEWAY, ifname))
 os.system("ip link set dev {} up".format(ifname))
 
-# Create UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((CLIENT_IP, CLIENT_PORT))
-
 os.system("ip route add {} dev {} via {}".format(PRIVATE_NETWORK_SUBNET,ifname, CLIENT_TUN_GATEWAY))
 
+# Create UDP socket
+ingress_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ingress_context.load_cert_chain(certfile=PATH_CERT_FILE, keyfile=PATH_KEY_FILE)
+ingress_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ingress_socket.bind((CLIENT_IP, CLIENT_PORT))
+ingress_socket.listen(1)
+
+egress_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT, check_hostname=False)
+egress_context.load_verify_locations(PATH_CERT_FILE)
+egress_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# egress_tls_socket = egress_context.wrap_socket(egress_socket)
+egress_tls_socket = egress_context.wrap_socket(egress_socket, server_hostname=SERVER_IP)
 while True:
-    ready, _, _ = select.select([sock, tun], [], [])
+    try:
+        egress_tls_socket.connect((SERVER_IP, SERVER_PORT))
+        break
+    except Exception as e:
+        print(e)
+        time.sleep(1)
+
+ingress_connection, fromaddr = ingress_socket.accept()
+# ingress_tls_connection = ingress_context.wrap_socket(ingress_connection, server_side=True)
+print("accept conn from {}".format(fromaddr))
+
+# sock.sendall(b'Hello, world')
+# context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+# context.load_cert_chain(certfile=PATH_CERT_FILE, keyfile=PATH_KEY_FILE)
+
+while True:
+    ready, _, _ = select.select([ingress_connection, tun], [], [])
     for fd in ready:
-        if fd is sock:
-            data, (ip, port) = sock.recvfrom(2048)
+        if fd is ingress_connection:
+            data = ingress_connection.recv(2048)
+            # data, (ip, port) = sock.recvfrom(2048)
             pkt = IP(data)
             print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
             os.write(tun, data)
@@ -54,4 +78,11 @@ while True:
             packet = os.read(tun, 2048)
             pkt = IP(packet)
             print("From tun ==>: {} --> {}".format(pkt.src, pkt.dst))
-            sock.sendto(packet, (SERVER_IP, SERVER_PORT))
+            # sock.connect((SERVER_IP, SERVER_PORT))
+            # sock.connect((SERVER_IP, SERVER_PORT))
+            # sock.send(packet)
+            print("sending")
+            # sock.sendall(b'Hello, world')
+            egress_tls_socket.send(bytes(pkt))
+            # sock.sendto(packet, (SERVER_IP, SERVER_PORT))
+            # sock.close()
--- network/Labsetup/volumes/tun_server.py
+++ network/Labsetup/volumes/tun_server.py
@@ -5,6 +5,7 @@ import struct
 import os
 import time
 from scapy.all import *
+import ssl
 
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
@@ -16,39 +17,69 @@ PROTO_NUMBER_ICMP = 1
 CLIENT_IP="10.9.0.5"
 CLIENT_PORT = 9090
 CLIENT_TUN_GATEWAY="192.168.53.99"
-
 SERVER_IP="10.9.0.11"
 SERVER_PORT=9090
 SERVER_TUN_GATEWAY="192.168.53.98"
-
 PRIVATE_NETWORK_SUBNET="192.168.60.0/24"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((SERVER_IP, SERVER_PORT))
+PATH_CERT_FILE="cert.pem"
+PATH_KEY_FILE="key.pem"
 
 # Create the tun interface
 tun = os.open("/dev/net/tun", os.O_RDWR)
 ifr = struct.pack('16sH', b'tun%d', IFF_TUN | IFF_NO_PI)
 ifname_bytes  = fcntl.ioctl(tun, TUNSETIFF, ifr)
-
-# Get the interface name
 ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
 print("Interface Name: {}".format(ifname))
-
 os.system("ip addr add {}/24 dev {}".format(SERVER_TUN_GATEWAY, ifname))
 os.system("ip link set dev {} up".format(ifname))
 
+# Create Socket
+ingress_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ingress_context.load_cert_chain(certfile=PATH_CERT_FILE, keyfile=PATH_KEY_FILE)
+ingress_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ingress_socket.bind((SERVER_IP, SERVER_PORT))
+ingress_socket.listen(1)
+
+egress_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT, check_hostname=False)
+egress_context.load_verify_locations(PATH_CERT_FILE)
+egress_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# egress_tls_socket = egress_context.wrap_socket(egress_socket)
+# egress_tls_socket = egress_context.wrap_socket(egress_socket, server_hostname=CLIENT_IP)
 while True:
-    ready, _, _ = select.select([sock, tun], [], [])
+    try:
+        egress_socket.connect((CLIENT_IP, CLIENT_PORT))
+        break
+    except Exception as e:
+        print(e)
+        time.sleep(1)
+
+ingress_connection, fromaddr = ingress_socket.accept()
+ingress_tls_connection = ingress_context.wrap_socket(ingress_connection, server_side=True)
+print("accept conn from {}".format(fromaddr))
+
+# data= conn.recv(2048)
+# print(data)
+while True:
+    ready, _, _ = select.select([ingress_tls_connection, tun], [], [])
     for fd in ready:
-        if fd is sock:
-            data, (ip, port) = sock.recvfrom(2048)
+        if fd is ingress_tls_connection:
+            # conn, fromaddr = sock.accept()
+            # print("accept conn from {}".format(fromaddr))
+            # connstream = context.wrap_socket(sslsocket, server_side=True)
+            # connstream, fromaddr = sock.accept()
+            data = ingress_tls_connection.recv(2048)
+            # data, (ip, port) = conn.recvfrom(2048)
             pkt = IP(data)
             print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
+            pkt.show()
             os.write(tun, data)
+            # conn.shutdown(socket.SHUT_RDWR)
+            # conn.close()
         
         if fd is tun:
             packet = os.read(tun, 2048)
             pkt = IP(packet)
             print("From tun ==>: {} --> {}".format(pkt.src, pkt.dst))
-            sock.sendto(packet, (CLIENT_IP, CLIENT_PORT))
+            egress_socket.send(bytes(pkt))
+            # sock.sendto(packet, (CLIENT_IP, CLIENT_PORT))
